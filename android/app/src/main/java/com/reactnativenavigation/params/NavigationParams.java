3b7f951694eeb0f0a02d73e4cd4a08952b58ba48
Edward Drapkin <edward.drapkin@flexshopper.com>
2018-05-09 10:53:23 -0400

    Add isSimulator to navigation props

--- android/app/src/main/java/com/reactnativenavigation/params/NavigationParams.java
+++ android/app/src/main/java/com/reactnativenavigation/params/NavigationParams.java
@@ -1,6 +1,7 @@
 package com.reactnativenavigation.params;
 
 import android.os.Bundle;
+import android.os.Build;
 
 public class NavigationParams {
     public static final NavigationParams EMPTY = new NavigationParams(Bundle.EMPTY);
@@ -8,10 +9,21 @@ public class NavigationParams {
     private static final String SCREEN_INSTANCE_ID = "screenInstanceID";
     private static final String NAVIGATOR_ID = "navigatorID";
     private static final String NAVIGATOR_EVENT_ID = "navigatorEventID";
+    private static final String IS_SIMULATOR_ID = "isSimulator";
+
+    private static final boolean IS_SIMULATOR = Build.FINGERPRINT.startsWith("generic")
+            || Build.FINGERPRINT.startsWith("unknown")
+            || Build.MODEL.contains("google_sdk")
+            || Build.MODEL.contains("Emulator")
+            || Build.MODEL.contains("Android SDK built for x86")
+            || Build.MANUFACTURER.contains("Genymotion")
+            || (Build.BRAND.startsWith("generic") && Build.DEVICE.startsWith("generic"))
+            || "google_sdk".equals(Build.PRODUCT);
 
     public String screenInstanceId;
     public String navigatorId;
     public String navigatorEventId;
+
 
     public NavigationParams(Bundle bundle) {
         screenInstanceId = bundle.getString(SCREEN_INSTANCE_ID);
@@ -24,6 +36,7 @@ public class NavigationParams {
         b.putString(SCREEN_INSTANCE_ID, screenInstanceId);
         b.putString(NAVIGATOR_ID, navigatorId);
         b.putString(NAVIGATOR_EVENT_ID, navigatorEventId);
+        b.putBoolean(IS_SIMULATOR_ID, IS_SIMULATOR);
         return b;
     }
 }
