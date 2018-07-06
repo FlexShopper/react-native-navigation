6ee38edd0af64ff67b085245d67b2d5be778c4a3
Leo Stanley <leo.stanley@flexshopper.com>
2018-07-06 19:10:28 -0400

    add method to convert hex strings to UIColor

--- ios/RCCViewController.m
+++ ios/RCCViewController.m
@@ -324,6 +324,18 @@ const NSInteger TRANSPARENT_NAVBAR_TAG = 78264803;
     [self setStyleOnAppearForViewController:self appeared:true];
 }
 
+- (UIColor *)hexToUIColor:(NSString *)hexString {
+    unsigned rgbValue = 0;
+    NSScanner *scanner = [NSScanner scannerWithString:hexString];
+    [scanner setScanLocation:1];
+    [scanner scanHexInt:&rgbValue];
+    return [UIColor 
+      colorWithRed:((rgbValue & 0xFF0000) >> 16)/255.0 
+      green:((rgbValue & 0xFF00) >> 8)/255.0 
+      blue:(rgbValue & 0xFF)/255.0 
+      alpha:1.0];
+}
+
 -(void)setStyleOnAppearForViewController:(UIViewController*)viewController appeared:(BOOL)appeared {
     NSString *screenBackgroundColor = self.navigatorStyle[@"screenBackgroundColor"];
     if (screenBackgroundColor) {
